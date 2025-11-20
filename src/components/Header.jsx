cd7c9adec94b159d670bf18ce03333fdfc4dfa41
Tim Kmecl <tim.kmecl.tk@gmail.com>
2025-11-20 15:00:42 +0100

    Fix header icon sizing and alignment on mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="App-header">
       <div className="header-content">
-        <TicketIcon size={32} className="header-icon" />
+        <TicketIcon className="header-icon" />
         <h1>Javni dogodki</h1>
       </div>
     </header>
